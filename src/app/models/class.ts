e02f06d9902f5878eb4decf859fd3f43834c6907
Gabriel Nicolae <gabriel.nicolae313@gmail.com>
2020-05-02 03:21:55 +0300

    class

---
+++ src/app/models/class.ts
@@ -0,0 +1,10 @@
+import { Major } from './major';
+export class Class {
+    id: number;
+    name: string;
+    creditPoints: number;
+    type: string;
+    year: number;
+    semester: number;
+    major: Major;
+}
---
+++ src/app/services/class.service.ts
@@ -0,0 +1,27 @@
+import { Class } from './../models/class';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClassService {
+
+  constructor(private http: HttpClient) {}
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('localJWT')).jwt
+    })
+  };
+
+  private endpoint = '/classes';
+  private hostname = 'http://aws-backend-ipapp.eba-g7bht3us.eu-central-1.elasticbeanstalk.com';
+
+  getClasses(): Observable<Class[]>{
+    return this.http.get(this.hostname + this.endpoint, this.httpOptions) as Observable<Class[]>;
+  }
+
+}
